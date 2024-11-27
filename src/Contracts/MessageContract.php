9c4fc52c893f4fbcb54b2ded85685193b7c261a7
risetechapps <apps@risetech.com.br>
2024-11-27 10:50:54 -0300

    Atualizado contract

--- src/Contracts/MessageContract.php
+++ src/Contracts/MessageContract.php
@@ -3,10 +3,13 @@
 namespace RiseTechApps\Notify\Contracts;
 
 use RiseTechApps\Notify\Message\MessageEmailNotify;
+use RiseTechApps\Notify\Message\MessageSMSNotify;
 
 interface MessageContract
 {
     public function notifyEMail(): MessageEmailNotify;
-    public function notifySMS(): static;
+
+    public function notifySMS(): MessageSMSNotify;
+
     public function payload(array $payload): static;
 }
